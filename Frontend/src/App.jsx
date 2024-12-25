fa7effc02fe71ce28e6f38c26173acb6660a99e4
Dev Agrawal <agrawaldev261004@gmail.com>
2024-12-25 22:22:53 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+
+function App() {
+ 
+  return (
+   <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<LandingPage />} />
+      </Routes>
+      </BrowserRouter>
+   </div>
+  )
+}
+
+export default App
